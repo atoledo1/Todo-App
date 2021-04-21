772f45c27e68ca91ab5b16820221a396a3caec7e
Adriana Toledo Santiago <atoledosantiago1@gmail.com>
2021-04-20 22:38:51 -0500

    penultimo

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React from "react";
 import "../App.css";
-import { WiredInput, WiredButton } from 'react-wired-elements';
-
+import { WiredInput, WiredButton } from "react-wired-elements";
 
 export default function AddTodo(props) {
-    return (
-        <div className="add-todo">
+  return (
+    <div className="add-todo">
+      <WiredInput
+        className="borde"
+        type="text"
+        name="currentTodo"
+        value={props.currentTodo}
+        onChange={props.handleChanges}
+        placeholder="New Task"
+      />
 
-            <WiredInput className="borde"
-
-    type="text"
-    name="currentTodo"
-    value={props.currentTodo}
-    onChange={props.handleChanges}
-                        placeholder="New Task"
-    />
-
-
-            <WiredButton onClick={props.handleSubmit}>Add Item</WiredButton>
-            <WiredButton onClick={props.clearCompleted}>Clear Completed</WiredButton>
-        </div>
-    );
+      <WiredButton onClick={props.handleSubmit}>Add Item</WiredButton>
+      <WiredButton onClick={props.clearCompleted}>Clear Completed</WiredButton>
+    </div>
+  );
 }
